955b4917d44ec7573c65506d64304f9cb1c9754a
sungsoo-eun <sungsoo.eun@posgen.net>
2017-06-28 15:27:48 +0900

    promise example

---
+++ promise/app1.js
@@ -0,0 +1,36 @@
+//Promise 선언
+var _promise = function (param) {
+
+	return new Promise(function (resolve, reject) {
+
+		// 비동기를 표현하기 위해 setTimeout 함수를 사용
+		setTimeout(function () {
+
+			// 파라메터가 참이면,
+			if (param) {
+
+				// 해결됨
+				resolve("성공!!");
+			}
+
+			// 파라메터가 거짓이면,
+			else {
+
+				// 실패
+				reject(Error("실패!!"));
+				//reject("실패!!");
+			}
+		}, 3000);
+	});
+};
+
+//Promise 실행
+_promise(true)
+//_promise(false)
+.then(function (text) {
+	// 성공시
+	console.log(text);
+}, function (error) {
+	// 실패시
+	console.error(error);
+});
